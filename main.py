896405a9880f907859baf1e579dfc39f02621b8b
Mainak Kundu <mainak1729@gmail.com>
2013-11-09 10:44:56 +0530

    Refactoring

--- main.py
+++ main.py
@@ -11,16 +11,6 @@ from CommonData import cityList
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-minDate = '2008-01-01'
-maxDate = '2013-10-19'
-
-firstForm = jinja_env.get_template('form.html').render(cityList=cityList,
-    cityName='Ahmedabad',
-    startDate='2013-01-01',
-    endDate='2013-10-19',
-    minDate=minDate,
-    maxDate=maxDate)
-
 class Isp(object):
     def __init__(self, isp_name, download_kbps, upload_kbps, total_tests, distance_kms):
         self.ispName = isp_name
@@ -29,6 +19,98 @@ class Isp(object):
         self.uploadKbps = upload_kbps
         self.totalTests = total_tests
         self.distanceKms = distance_kms
+
+minDate = '2008-01-01'
+maxDate = '2013-10-19'
+
+def dateStringToDate(dateString): #for YYYY-MM-DD
+    return datetime.date(*map(lambda x: int(x), dateString.split('-')))
+
+def dateRangeToDates(dateRange): #for 'YYYY-MM-DD to YYYY-MM-DD'
+    return map(dateStringToDate, dateRange.split(' to '))
+
+def generateDataRows(ispList, attribute):
+    return [[ isp.ispName,
+        getattr(isp, attribute),
+        isp.ispName.upper()
+            + '\nAverage download speed: '
+            + '{0:.3f}'.format(isp.downloadKbps)
+            + ' kbps\nAverage upload speed: '
+            + '{0:.3f}'.format(isp.uploadKbps)
+            + ' kbps\nNumber of tests analysed: '
+            + str(isp.totalTests)
+            + '\nAverage distance between the client and the server across all tests: '
+            + '{0:.3f}'.format(isp.distanceKms)
+            + ' km'] for isp in ispList]
+
+def generateChartData(cityName, startDate, endDate):
+    #cityDataFile is a csv file containing ISP name, test date, average download speed (kbps), average upload speed (kbps), number of tests analysed, average distance (miles) between the client and the server across all tests. For example,
+
+    #ahmedabadData.py:
+    #"NIB (National Internet Backbone)","2008-01-01",1549.52,237.856,272,283.735
+
+    cityDataFile = cityName.lower() + 'Data.csv'
+    cityData = open(cityDataFile, 'r')
+
+    ispList = []
+    ispNameList = []
+
+    dataIterator = csv.reader(cityData, skipinitialspace=True)
+    currentData = dataIterator.next()
+
+    while dateStringToDate(currentData[1]) < startDate:
+        currentData = dataIterator.next()
+
+    while dateStringToDate(currentData[1]) <= endDate:
+        try:
+            currentIspName = currentData[0]
+            if currentIspName not in ispNameList:
+                ispList.append(Isp(currentIspName, float(currentData[2]), float(currentData[3]), int(currentData[4]), float(currentData[5])*1.60934))
+                ispNameList.append(currentIspName)
+            else:
+                currentIndex = ispNameList.index(currentIspName)
+                ispList[currentIndex].count += 1
+                ispList[currentIndex].downloadKbps += float(currentData[2])
+                ispList[currentIndex].uploadKbps += float(currentData[3])
+                ispList[currentIndex].totalTests += int(currentData[4])
+                ispList[currentIndex].distanceKms += float(currentData[5])*1.60934
+            currentData = dataIterator.next()
+        except StopIteration:
+            break
+
+    cityData.close()
+
+    for isp in ispList:
+        isp.downloadKbps /= isp.count
+        isp.uploadKbps /= isp.count
+        isp.distanceKms /= isp.count
+
+    ispListSortedByDownloadKbps = sorted(ispList, key=operator.attrgetter('downloadKbps'), reverse=True)
+    ispListSortedByUploadKbps = sorted(ispList, key=operator.attrgetter('uploadKbps'), reverse=True)
+    dataRowsForDownloadSpeed = generateDataRows(ispListSortedByDownloadKbps, 'downloadKbps')
+    dataRowsForUploadSpeed = generateDataRows(ispListSortedByUploadKbps, 'uploadKbps')
+
+    return dataRowsForDownloadSpeed, dataRowsForUploadSpeed
+
+firstCityName = 'Ahmedabad'
+firstStartDate = '2013-01-01'
+firstEndDate = '2013-10-19'
+firstDataRowsForDownloadSpeed, firstDataRowsForUploadSpeed = generateChartData(firstCityName, dateStringToDate(firstStartDate), dateStringToDate(firstEndDate))
+
+firstForm = jinja_env.get_template('form.html').render(cityList=cityList,
+    cityName=firstCityName,
+    startDate=firstStartDate,
+    endDate=firstEndDate,
+    minDate=minDate,
+    maxDate=maxDate)
+
+firstChart = jinja_env.get_template('chart.html').render(cityName=firstCityName,
+    startDate=firstStartDate,
+    endDate=firstEndDate,
+    dataRowsForDownloadSpeed=json.dumps(firstDataRowsForDownloadSpeed),
+    dataRowsForUploadSpeed=json.dumps(firstDataRowsForUploadSpeed),
+    ispCount=len(firstDataRowsForDownloadSpeed),
+    maxDate=maxDate)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *args, **keywords):
@@ -40,41 +122,14 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **keywords))
 
 class MainPage(Handler):
-    def dateStringToDate(self, dateString): #for YYYY-MM-DD
-        return datetime.date(*map(lambda x: int(x), dateString.split('-')))
-
-    def dateRangeToDates(self, dateRange): #for 'YYYY-MM-DD to YYYY-MM-DD'
-        return map(self.dateStringToDate, dateRange.split(' to '))
-
-    def generateDataRows(self, ispList, attribute):
-        return [[ isp.ispName,
-            getattr(isp, attribute),
-            isp.ispName.upper()
-                + '\nAverage download speed: '
-                + '{0:.3f}'.format(isp.downloadKbps)
-                + ' kbps\nAverage upload speed: '
-                + '{0:.3f}'.format(isp.uploadKbps)
-                + ' kbps\nNumber of tests analysed: '
-                + str(isp.totalTests)
-                + '\nAverage distance between the client and the server across all tests: '
-                + '{0:.3f}'.format(isp.distanceKms)
-                + ' km'] for isp in ispList]
-
     def get(self):
         self.response.out.write(firstForm)
+        self.response.out.write(firstChart)
 
     def post(self):
         cityName = self.request.get('city')
-        '''
-        cityDataFile is a csv file containing ISP name, test date, average download speed (kbps), average upload speed (kbps), number of tests analysed, average distance (miles) between the client and the server across all tests. For example,
 
-        ahmedabadData.py:
-        "NIB (National Internet Backbone)","2008-01-01",1549.52,237.856,272,283.735
-        '''
-        cityDataFile = cityName.lower() + 'Data.csv'
-        cityData = open(cityDataFile, 'r')
-
-        startDate, endDate = self.dateRangeToDates(self.request.get('dateRange'))
+        startDate, endDate = dateRangeToDates(self.request.get('dateRange'))
 
         self.render('form.html',
             cityList=cityList,
@@ -84,43 +139,7 @@ class MainPage(Handler):
             minDate=minDate,
             maxDate=maxDate)
 
-        ispList = []
-        ispNameList = []
-
-        dataIterator = csv.reader(cityData, skipinitialspace=True)
-        currentData = dataIterator.next()
-
-        while self.dateStringToDate(currentData[1]) < startDate:
-            currentData = dataIterator.next()
-
-        while self.dateStringToDate(currentData[1]) <= endDate:
-            try:
-                currentIspName = currentData[0]
-                if currentIspName not in ispNameList:
-                    ispList.append(Isp(currentIspName, float(currentData[2]), float(currentData[3]), int(currentData[4]), float(currentData[5])*1.60934))
-                    ispNameList.append(currentIspName)
-                else:
-                    currentIndex = ispNameList.index(currentIspName)
-                    ispList[currentIndex].count += 1
-                    ispList[currentIndex].downloadKbps += float(currentData[2])
-                    ispList[currentIndex].uploadKbps += float(currentData[3])
-                    ispList[currentIndex].totalTests += int(currentData[4])
-                    ispList[currentIndex].distanceKms += float(currentData[5])*1.60934
-                currentData = dataIterator.next()
-            except StopIteration:
-                break
-
-        cityData.close()
-
-        for isp in ispList:
-            isp.downloadKbps /= isp.count
-            isp.uploadKbps /= isp.count
-            isp.distanceKms /= isp.count
-
-        ispListSortedByDownloadKbps = sorted(ispList, key=operator.attrgetter('downloadKbps'), reverse=True)
-        ispListSortedByUploadKbps = sorted(ispList, key=operator.attrgetter('uploadKbps'), reverse=True)
-        dataRowsForDownloadSpeed = self.generateDataRows(ispListSortedByDownloadKbps, 'downloadKbps')
-        dataRowsForUploadSpeed = self.generateDataRows(ispListSortedByUploadKbps, 'uploadKbps')
+        dataRowsForDownloadSpeed, dataRowsForUploadSpeed = generateChartData(cityName, startDate, endDate)
 
         self.render('chart.html',
             cityName=cityName,
@@ -128,7 +147,7 @@ class MainPage(Handler):
             endDate=str(endDate),
             dataRowsForDownloadSpeed=json.dumps(dataRowsForDownloadSpeed),
             dataRowsForUploadSpeed=json.dumps(dataRowsForUploadSpeed),
-            ispCount=len(ispList),
+            ispCount=len(dataRowsForDownloadSpeed),
             maxDate=maxDate)
 
 app = webapp2.WSGIApplication([('/', MainPage)])
